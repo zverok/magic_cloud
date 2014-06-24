b139af409e7a335f5f16ed6b90977dcd6460a78f
zverok <zverok.offline@gmail.com>
2014-06-24 10:54:01 +0300

    Should not need this

--- lib/magic_cloud/core_ext.rb
+++
@@ -1,7 +0,0 @@
-# as per http://stackoverflow.com/questions/3476969/rotate-bits-right-operation-in-ruby
-# imitating JS's >>>
-class Integer
-  def ror(count)
-    (self >> count) | (self << (32 - count)) & 0xFFFFFFFF
-  end
-end
